ae438e43f2f0d046b92158ff4edbadf2b0627ae2
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-05-16 02:06:59 +0100

    Change token form integer for string

--- user/forms.py
+++ user/forms.py
@@ -4,4 +4,4 @@ class UserForm(forms.Form):
     username = forms.CharField(label='username', max_length=200)
     password = forms.CharField(label='password', max_length=200)
     password_confirm = forms.CharField(label='password_confirm', max_length=200, required=False)
-    totp_token = forms.IntegerField(label='totp_token', max_value=999999, required=False)
+    totp_token = forms.CharField(label='totp_token', max_value=6, required=False)
