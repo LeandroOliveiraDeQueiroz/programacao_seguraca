243ea6e512df7fbfc35f168131f07f0032b1d6e0
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-05-16 02:09:34 +0100

    .

--- user/forms.py
+++ user/forms.py
@@ -4,4 +4,4 @@ class UserForm(forms.Form):
     username = forms.CharField(label='username', max_length=200)
     password = forms.CharField(label='password', max_length=200)
     password_confirm = forms.CharField(label='password_confirm', max_length=200, required=False)
-    totp_token = forms.CharField(label='totp_token', max_value=6, required=False)
+    totp_token = forms.CharField(label='totp_token', max_length=6, required=False)
